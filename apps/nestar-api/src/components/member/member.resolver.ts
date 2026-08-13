a2597ef7c2c818418448e76cc23c84f7c2af4183
Sam <yuldashevsamandar9@gmail.com>
2026-08-13 21:22:34 +0900

    feat: memberSchemamodel create

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -7,25 +7,25 @@ export class MemberResolver {
 
 	@Mutation(() => String)
 	public async signup(): Promise<string> {
-		console.log('Mutation:, signup');
+		console.log('Mutation: signup');
 		return this.memberService.signup();
 	}
 
 	@Mutation(() => String)
 	public async login(): Promise<string> {
-		console.log('Mutation:,login');
+		console.log('Mutation: login');
 		return this.memberService.login();
 	}
 
 	@Mutation(() => String)
 	public async updateMember(): Promise<string> {
-		console.log('Mutation:, updateMember');
+		console.log('Mutation: updateMember');
 		return this.memberService.updateMember();
 	}
 
 	@Query(() => String)
 	public async getMember(): Promise<string> {
-		console.log('Query:, getMember');
+		console.log('Query: getMember');
 		return this.memberService.getMember();
 	}
 }
