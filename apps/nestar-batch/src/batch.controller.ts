e4935c3d37c7a792a773c96287f211c6823ec688
Sam <yuldashevsamandar9@gmail.com>
2026-09-09 11:56:36 +0900

    feat: Create Jboschedule

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,9 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
 
 @Controller()
 export class BatchController {
-	constructor(private readonly BatchService: BatchService) {}
+	private logger: Logger = new Logger('BatchController');
+	[x: string]: any;
+	constructor(private readonly batchService: BatchService) {}
+
+	// @Interval(1000)
+	@Timeout(1000)
+	handleInterval() {
+		this.logger.debug('BATCH SERVER READY');
+	}
+	@Cron('00 * * * * * ', { name: 'batchRollback' })
+	public async batchRollback() {
+		try {
+			this.logger['Context'] = 'batchRollback ';
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchRollback();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('20  * * * * * ', { name: 'BATCH_TOP_PROPERTIES' })
+	public async batchProperties() {
+		try {
+			this.logger['Context'] = 'BATCH_TOP_PROPERTIES';
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchProperties();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('40  * * * * * ', { name: 'BATCH_TOP_AGENTS' })
+	public async batchTopAgents() {
+		try {
+			this.logger['Context'] = 'BATCH_TOP_AGENTS';
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchTopAgents();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
 
 	@Get()
 	getHello(): string {
--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -2,6 +2,18 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BatchService {
+	public async batchRollback(): Promise<void> {
+		console.log('batchRollback');
+	}
+
+	public async batchProperties(): Promise<void> {
+		console.log('batchProperties');
+	}
+
+	public async batchTopAgents(): Promise<void> {
+		console.log('batchTopAgents');
+	}
+
 	getHello(): string {
 		return 'Hello World NESTAR - BATCH Project';
 	}
